7bf9bb6350c6abb6336b77a82ca908e3dc742c9c
Gweltaz <gweltazmary@gmail.com>
2023-09-04 11:12:17 +0200

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+export default function HomePage() {
+	return (
+		<div>
+			<h1>Welcome to my WebSite</h1>
+			<ul>
+				<li>
+					<Link href='/'>Home</Link>
+				</li>
+				<li>
+					<Link href='/about'>About</Link>
+				</li>
+				<li>
+					<Link href='/about/team'>Team</Link>
+				</li>
+			</ul>
+		</div>
+	);
+}
